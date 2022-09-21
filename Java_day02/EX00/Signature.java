72816efb4f598cf1c855d4db7f1cf0e96eb35b72
EMIR1HUB <emir-23.03@mail.ru>
2022-09-21 16:54:00 +0300

    add 02/ex01

---
+++ Java_day02/EX00/Signature.java
@@ -0,0 +1,9 @@
+import java.io.FileInputStream;
+import java.security.Signature;
+
+public class Sgnature {
+
+    public static Signature createFromSignatureFile(FileInputStream inputStream){
+
+    }
+}
